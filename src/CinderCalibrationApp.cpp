008d05ec8a56d8468e534fa4ab2d704823800e77
Keith Butters <keith@barbariangroup.com>
2013-10-30 18:09:01 -0400

    refactoring

--- src/CinderCalibrationApp.cpp
+++ src/CinderCalibrationApp.cpp
@@ -16,10 +16,6 @@
 #define CAPTURE_WIDTH		640		// pixel width of camera input
 #define CAPTURE_HEIGHT		480		// pixel height of camera input
 
-#define STATE_DETECT		0
-#define STATE_TAKE_IMG		1
-#define STATE_CALIBRATING	2
-#define STATE_CALIBRATED	3
 
 #define OUTPUT_FILE	"Users/keith/Desktop/params.yaml"
 
@@ -35,12 +31,20 @@ class CinderCalibrationApp : public AppNative {
 	void keyDown( KeyEvent event );
 	void update();
 	void draw();
-	bool saveCameraParams();
+	
+	void	collectImages();
+	bool	callibrate();
+	bool	saveCameraParams();
+	double	getAvgError() const;
+	void	reset();
+	
+	enum states { STATE_DETECT, STATE_TAKE_IMG, STATE_CALIBRATING, STATE_CALIBRATED };
 	
 	CaptureRef				mCapture;
 	Surface					mCaptureSurf;
 	cinder::gl::TextureRef	mCaptureTex;
 	
+  private:
 	Mat mCaptureMat;
 	Mat mGrayMat;
 	Mat mUndistortedMat;
@@ -56,9 +60,8 @@ class CinderCalibrationApp : public AppNative {
 	int mState;
 	int mImages;
 	
-	bool showDistorted;
-	double avgError;
-
+	double	mAvgError;
+	bool	showDistorted;
 };
 
 void CinderCalibrationApp::prepareSettings( cinder::app::AppBasic::Settings *settings ) {
@@ -71,6 +74,7 @@ void CinderCalibrationApp::setup()
 	mState			= STATE_DETECT;
 	mImages			= 0;
 	showDistorted	=	true;
+	mAvgError		= 0;
 
 	try {
 		mCapture = Capture::create( CAPTURE_WIDTH, CAPTURE_HEIGHT );
@@ -102,6 +106,60 @@ void CinderCalibrationApp::keyDown( KeyEvent event )
 	}
 }
 
+void CinderCalibrationApp::collectImages()
+{
+	cv::Size boardSize = cv::Size( BOARD_CORNERS_X, BOARD_CORNERS_Y );
+	cvtColor( mCaptureMat, mGrayMat, CV_BGR2GRAY );
+	vector<Point2f> corners;
+
+	bool found = findChessboardCorners( mCaptureMat, boardSize, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS );
+
+	if ( found ) {
+		cornerSubPix( mGrayMat, corners, cv::Size( 11, 11 ), cv::Size( -1, -1 ), cv::TermCriteria( CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1 ) );
+		drawChessboardCorners( mGrayMat, boardSize, Mat(corners), true );
+	}
+
+	// capture image and object points for callibration
+	if ( found && mState == STATE_TAKE_IMG ) {
+		
+		mImagePoints.push_back( corners );
+		mObjectPoints.push_back( obj );
+		mImages++;
+		console() << "image " << mImages << " of " << BOARD_IMG_COUNT << endl;
+		
+		if ( mImages >= BOARD_IMG_COUNT ) {
+			mState = STATE_CALIBRATING;
+		} else {
+			mState = STATE_DETECT;
+		}
+	}
+}
+
+bool CinderCalibrationApp::callibrate()
+{
+	// Calibrate
+	intrinsic = Mat(3, 3, CV_32FC1);
+	vector<Mat> rvecs;
+	vector<Mat> tvecs;
+	intrinsic.ptr<float>(0)[0] = CAPTURE_WIDTH / CAPTURE_HEIGHT;
+	intrinsic.ptr<float>(1)[1] = 1;
+	mAvgError = calibrateCamera( mObjectPoints, mImagePoints, mCaptureMat.size(), intrinsic, distortion, rvecs, tvecs );
+
+	// console() << "re-projection error from calibrateCamera(): " << mAvgError << endl;
+
+	if ( !checkRange( intrinsic ) || !checkRange( distortion ) ) {
+		console() << "Calibration failed. Try again." << endl;
+		return false;
+	}
+	return true;
+}
+
+double CinderCalibrationApp::getAvgError() const
+{
+	return mAvgError;
+}
+
+
 bool CinderCalibrationApp::saveCameraParams()
 {
 	ofstream oStream( "/Users/keith/Desktop/params.yaml" );
@@ -117,82 +175,36 @@ bool CinderCalibrationApp::saveCameraParams()
 	oStream << "distortion_coefficients: !!opencv-matrix\n";
 	oStream << "   rows: 1\n   cols: 5\n   dt: d\n";
 	oStream << "   data:" << distortion << "\n";
-	oStream << "avg_reprojection_error: " << avgError << "\n";
+	oStream << "avg_reprojection_error: " << mAvgError << "\n";
 	
 	oStream.close();
 	return true;
 }
 
-
+void CinderCalibrationApp::reset()
+{
+	// reset state and clear objects
+}
 
 void CinderCalibrationApp::update()
 {
 	if ( mCapture->checkNewFrame() ) {
-		
-		// Find the chessboard in the video feed
 		mCaptureSurf	= mCapture->getSurface();
 		mCaptureTex		= gl::Texture::create( mCaptureSurf );
 		mCaptureMat		= toOcv( mCaptureSurf );
 		
-		cv::Size boardSize = cv::Size( BOARD_CORNERS_X, BOARD_CORNERS_Y );
-		cvtColor( mCaptureMat, mGrayMat, CV_BGR2GRAY );
-		vector<Point2f> corners;
+		collectImages();
 		
-		bool found = findChessboardCorners( mCaptureMat, boardSize, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS );
-		
-		if ( found ) {
-			cornerSubPix( mGrayMat, corners, cv::Size( 11, 11 ), cv::Size( -1, -1 ), cv::TermCriteria( CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1 ) );
-			drawChessboardCorners( mGrayMat, boardSize, Mat(corners), true );
-		}
-		
-		// capture image and object points for callibration
-		if ( found && mState == STATE_TAKE_IMG ) {
-			
-			mImagePoints.push_back( corners );
-            mObjectPoints.push_back( obj );
-			mImages++;
-			console() << "image " << mImages << " of " << BOARD_IMG_COUNT << endl;
-			
-			if ( mImages >= BOARD_IMG_COUNT ) {
-				mState = STATE_CALIBRATING;
-			} else {
-				mState = STATE_DETECT;
-			}
-		}
-		
-		// calibrate camera and check for projection errors
-		if ( mState == STATE_CALIBRATING ) {
-			
-			// Calibrate
-			intrinsic = Mat(3, 3, CV_32FC1);
-			vector<Mat> rvecs;
-			vector<Mat> tvecs;
-			intrinsic.ptr<float>(0)[0] = CAPTURE_WIDTH / CAPTURE_HEIGHT;
-			intrinsic.ptr<float>(1)[1] = 1;
-			avgError = calibrateCamera( mObjectPoints, mImagePoints, mCaptureMat.size(), intrinsic, distortion, rvecs, tvecs );
-			
-			console() << "re-projection error from calibrateCamera(): " << avgError << endl;
-			
-			if ( !checkRange( intrinsic ) || !checkRange( distortion ) ) {
-				console() << "Calibration failed. Try again." << endl;
-				exit(1);
- 			}
-			// void calibrationMatrixValues(InputArray cameraMatrix, Size imageSize, double apertureWidth, double apertureHeight, double& fovx, double& fovy, double& focalLength, Point2d& principalPoint, double& aspectRatio)
-			double apertureWidth;
-			double apertureHeight;
-			double fovx;
-			double fovy;
-			double focalLength;
-			Point2d principalPoint;
-			double aspectRatio;
-			
-			calibrationMatrixValues( intrinsic, boardSize, apertureWidth, apertureHeight, fovx, fovy, focalLength, principalPoint, aspectRatio );
-			
-			mState = STATE_CALIBRATED;
-		}
-		
-		if ( mState == STATE_CALIBRATED ) {
-			undistort( mCaptureMat, mUndistortedMat, intrinsic, distortion );
+		switch ( mState ) {
+			case STATE_CALIBRATING:
+				if ( callibrate() ) {
+					mState = STATE_CALIBRATED;
+				} else {
+					console() << "Calibration failed." << endl;
+					exit(1);
+				}
+			case STATE_CALIBRATED:
+				undistort( mCaptureMat, mUndistortedMat, intrinsic, distortion );
 		}
 	}
 }
